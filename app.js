04b996f642792ddb9ff469ce3ac1a3fd638b0c38
albertricart <albertricartt@gmail.com>
2020-10-13 01:29:42 +0200

    Styles added

---
+++ app.js
@@ -0,0 +1,9 @@
+var primitive = document.getElementById("primitive_data");
+var complex = document.getElementById("complex_data");
+
+function openUrl(url){
+    window.open(url, '_blank');
+}
+
+primitive.addEventListener('click', () => openUrl("https://jsfiddle.net/albertricart/uayw4mv0/"));
+complex.addEventListener('click', () => openUrl("https://jsfiddle.net/albertricart/or8q6egd/"));
