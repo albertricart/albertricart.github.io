e82ac3aea645845a1ac9bf2ec68ea160ac95d55c
albertricart <albertricartt@gmail.com>
2021-02-01 01:15:08 +0100

    scroll fab working as intended

--- js/app.js
+++ js/app.js
@@ -1,25 +1,14 @@
 var fab = document.getElementById('fab');
 var header = document.getElementById('header');
 
-var initialPos = 0;
-
-window.onscroll = function () { manageScroll() };
+window.onscroll = manageScroll;
 
 
 
 function manageScroll() {
-    if (window.scrollY != initialPos) {
-
-        if (window.scrollY >= initialPos) {
-            fab.style.transform = "scale(0)";
-            // fab.style.transform = "translateX(90px)";
-        } else {
-            fab.style.transform = "scale(1)";
-            // fab.style.transform = "translateX(0px)";
-        }
-        initialPos = window.scrollY;
-
+    if (window.scrollY >= "150") {
+        fab.style.transform = "scale(1)";
+    } else {
+        fab.style.transform = "scale(0)";
     }
-
-
 }
